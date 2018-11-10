245d65978a28acc7456d47553e1a2130f414bdc1
Papy_Poc <stephane.morin4@orange.fr>
2018-11-10 12:16:49 +0100

    Update -> Alpha 0.2

--- sliderpoc/controllers/index.php
+++ sliderpoc/controllers/index.php
@@ -1,24 +1,14 @@
-<?php if (!defined('NEOFRAG_CMS')) exit;
-/**************************************************************************
-Copyright © 2015 Michaël BILCOT & Jérémy VALENTIN
+<?php
+/******************************
+Copyright © 2018 Stéphane Morin
+*****************************
+*/
 
-This file is part of NeoFrag.
+namespace NF\Widgets\Discord\Controllers;
 
-NeoFrag is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+use NF\NeoFrag\Loadables\Controllers\Widget as Controller_Widget;
 
-NeoFrag is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with NeoFrag. If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************/
-
-class w_sliderpoc_c_index extends Controller_Widget
+class Index extends Controller_Widget
 {
 	public function index($settings = [])
 	{
@@ -43,8 +33,3 @@ class w_sliderpoc_c_index extends Controller_Widget
 		}
 	}
 }
-
-/*
-NeoFrag Alpha 0.1.6
-./widgets/gallery/controllers/index.php
-*/
