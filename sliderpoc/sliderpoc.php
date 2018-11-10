31aed12d2ba23de5a2cd799fba0952d83ab9b619
Papy_Poc <stephane.morin4@orange.fr>
2018-11-10 12:09:38 +0100

    Update -> Neofrag Alpha 0.2

--- sliderpoc/sliderpoc.php
+++ sliderpoc/sliderpoc.php
@@ -1,36 +1,27 @@
-<?php if (!defined('NEOFRAG_CMS')) exit;
-/**************************************************************************
-Copyright © 2015 Michaël BILCOT & Jérémy VALENTIN
+<?php
+/**
+ * https://neofr.ag
+ * @author: Papy Poc <https://github.com/PapyPoc>
+ */
 
-This file is part of NeoFrag.
+namespace NF\Widgets\Discord;
 
-NeoFrag is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+use NF\NeoFrag\Addons\Widget;
 
-NeoFrag is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with NeoFrag. If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************/
-
-class w_sliderpoc extends Widget
+class Discord extends Widget
 {
-	public $title       = '{lang gallery}';
-	public $description = 'Slider d\'un album';
-	public $link        = 'http://www.neofrag.com';
-	public $author      = 'Jérémy Valentin <jeremy.valentin@neofrag.com>';
-	public $licence     = 'http://www.neofrag.com/license.html LGPLv3';
-	public $version     = '1.0';
-	public $nf_version  = 'Alpha 0.1.6.1';
-	public $path        = __FILE__;
+	protected function __info()
+	{
+		return [
+			'title'       => '{lang slider}',
+			'description' => 'Slider d\'un album',
+			'link'        => 'https://neofr.ag',
+			'author'      => 'Stéphane Morin',
+			'license'     => 'LGPLv3 <https://neofr.ag/license>',
+			'version'     => '3.0',
+			'depends'     => [
+				'neofrag' => 'Alpha 0.2'
+			]
+		];
+	}
 }
-
-/*
-NeoFrag Alpha 0.1.5
-./widgets/gallery/gallery.php
-*/
